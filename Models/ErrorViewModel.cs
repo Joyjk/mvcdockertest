19489f5cbe3320f835a4e47b981bea042d75e5c2
joyjk <krmkrjoy@gmail.com>
2022-11-09 15:11:32 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvcdocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
